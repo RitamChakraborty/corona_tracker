e13feff166c4bda015d47ee3309729f3c0fdceb8
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-03-17 21:18:18 +0530

    Remove debug banner for release

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Corona Tracker",
       theme: ThemeData(primaryColor: Colors.white, buttonColor: Colors.green),
       darkTheme: ThemeData.dark(),

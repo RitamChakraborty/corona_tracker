13c24536d31f7f399ed855d15ca730b16d315a58
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-04-18 11:19:58 +0530

    Update CountryModel Add newly added properties

--- lib/model/country_data.dart
+++ lib/model/country_data.dart
@@ -1,15 +1,33 @@
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
+/// Model for the `API`
+///
+/// [country] name of the country
+/// [cases] number of `COVID-19` cases
+/// [todayCases] number of `COVID-19` cases on that day
+/// [deaths] number of deaths
+/// [todayDeaths] number of deaths on that day
+/// [recovered] number of people recovered
+/// [active] number of active cases
+/// [critical] number of people who are still in critical condition
+/// [casesPerOneMillion] number of people affected in one million
+/// [deathsPerOneMillion] number of people died per million
+/// [totalTests] number of people has be tested for `COVID-19`
+/// [testsPerOneMillion] number of people tested per million
 class CountryData {
-  String country;
-  int cases;
-  int todayCases;
-  int deaths;
-  int todayDeaths;
-  int recovered;
-  int active;
-  int critical;
+  final String country;
+  final int cases;
+  final int todayCases;
+  final int casesPerOneMillion;
+  final int deaths;
+  final int todayDeaths;
+  final int deathsPerOneMillion;
+  final int recovered;
+  final int active;
+  final int critical;
+  final int totalTests;
+  final int testsPerOneMillion;
 
   CountryData(
       {this.country,
@@ -19,29 +37,23 @@ class CountryData {
       this.todayDeaths,
       this.recovered,
       this.active,
-      this.critical});
+      this.critical,
+      this.casesPerOneMillion,
+      this.deathsPerOneMillion,
+      this.totalTests,
+      this.testsPerOneMillion});
 
-  factory CountryData.fromJson(Map<String, dynamic> data) {
-    var countryData = CountryData(
-        country: data['country'],
-        cases: data['cases'],
-        todayCases: data['todayCases'],
-        deaths: data['deaths'],
-        todayDeaths: data['todayDeaths'],
-        recovered: data['recovered'],
-        active: data['active'],
-        critical: data['critical']);
-    print(countryData);
-    return countryData;
-  }
-
+  /// Return percentage of people died among total number of cases
   double get deathPercentage => deaths * 100 / cases;
 
+  /// Return percentage of people recovered among total number of cases
   double get recoveredPercentage => recovered * 100 / cases;
 
+  /// Return percentage of active cases among total number of cases
   double get activePercentage => active * 100 / cases;
 
-  double get criticalPercentage => critical * 100 / cases;
+  /// Return percentage of people in critical conditions among total active cases
+  double get criticalPercentage => critical * 100 / active;
 
   List<charts.Series<DataClass, String>> get seriesList => [
         charts.Series<DataClass, String>(
@@ -60,13 +72,12 @@ class CountryData {
               charts.ColorUtil.fromDartColor(Colors.green[(i + 1) * 2 * 100]),
         ),
       ];
-
-  @override
-  String toString() {
-    return country;
-  }
 }
 
+/// A model for the `flutter charts`
+///
+/// [name] name of the bar in the chart
+/// [value] value of the bar in the chart
 class DataClass {
   final String name;
   final double value;

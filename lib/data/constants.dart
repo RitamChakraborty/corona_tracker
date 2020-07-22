7a35f404489b77b359f387e883c3ebd1b7a18d6b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-07-22 20:29:52 +0530

    Starting fresh

--- lib/data/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const String GLOBAL_DATA_URL = "https://coronavirus-19-api.herokuapp.com/all";
-const String COUNTRY_DATA_URL =
-    "https://coronavirus-19-api.herokuapp.com/countries";
-const Color ACCENT_COLOR = Colors.green;
-const ScrollPhysics SCROLL_PHYSICS = ClampingScrollPhysics();
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-  import 'package:coronatracker/pages/home_page.dart';
-import 'package:coronatracker/provider/data_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,23 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     /// Add a [StreamProvider] to listen to [DataConnectionStatus] changes
-    return ChangeNotifierProvider<DataProvider>.value(
-      value: DataProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Corona Tracker",
-
-        /// Light theme
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.white,
-          accentColor: Colors.greenAccent,
-        ),
-
-        /// Dark theme
-        darkTheme: ThemeData(brightness: Brightness.dark),
-        home: HomePage(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: "Corona Tracker",
+      home: Material(),
     );
   }
 }

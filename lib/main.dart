bc41e2b11e0be17f7e589fee56d8c92d586c881f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-07-25 20:40:18 +0530

    Add all the assets in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coronatracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       title: "Corona Tracker",
-      home: Material(),
+      home: HomePage(),
     );
   }
 }

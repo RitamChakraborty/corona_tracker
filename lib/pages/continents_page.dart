726b037ed61f9bfc4f4469406f070918f633ac59
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-08-03 19:01:57 +0530

    Create basic continents page

--- lib/pages/continents_page.dart
+++ lib/pages/continents_page.dart
@@ -1,4 +1,7 @@
+import 'package:coronatracker/models/continent.dart';
+import 'package:coronatracker/providers/service_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ContinentsPage extends StatefulWidget {
   @override
@@ -9,11 +12,26 @@ class _ContinentsPageState extends State<ContinentsPage>
     with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
+    ServiceProvider serviceProvider = Provider.of<ServiceProvider>(context);
+    List<Continent> continents = serviceProvider.continents;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Continents"),
       ),
-      body: Container(),
+      body: continents == null ?
+          Center(
+            child: CircularProgressIndicator(),
+          ) : ListView.builder(
+        itemCount: continents.length,
+        itemBuilder: (BuildContext context, int index) {
+          Continent continent = continents[index];
+
+          return ListTile(
+            title: Text(continent.continent),
+          );
+        },
+      )
     );
   }
 

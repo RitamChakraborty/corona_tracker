ad256076d28a2a59f138a6fd8a63fbd98acdc00f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-08-05 22:15:34 +0530

    Create continent tile

--- lib/pages/continents_page.dart
+++ lib/pages/continents_page.dart
@@ -1,5 +1,6 @@
 import 'package:coronatracker/models/continent.dart';
 import 'package:coronatracker/providers/service_provider.dart';
+import 'package:coronatracker/widgets/continent_tile.dart';
 import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,26 +21,27 @@ class _ContinentsPageState extends State<ContinentsPage>
       child: SafeArea(
         child: continents == null
             ? Center(
-          child: CircularProgressIndicator(),
-        )
+                child: CircularProgressIndicator(),
+              )
             : FloatingSearchBar.builder(
-          leading: Icon(Icons.search),
-          trailing: IconButton(
-            icon: Icon(Icons.sort),
-            onPressed: null,
-          ),
-          padding:
-          const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-          decoration:
-          InputDecoration.collapsed(hintText: "Search country"),
-          itemCount: continents.length,
-          itemBuilder: (BuildContext context, int index) {
-            Continent continent = continents[index];
-            return ListTile(
-              title: Text("${continent.continent}"),
-            );
-          },
-        ),
+                leading: Icon(Icons.search),
+                trailing: IconButton(
+                  icon: Icon(Icons.sort),
+                  onPressed: null,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration:
+                    InputDecoration.collapsed(hintText: "Search country"),
+                itemCount: continents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Continent continent = continents[index];
+                  return ContinentTile(
+                    continent: continent,
+                    index: index,
+                  );
+                },
+              ),
       ),
     );
   }
---
+++ lib/widgets/continent_tile.dart
@@ -0,0 +1,51 @@
+import 'package:coronatracker/models/continent.dart';
+import 'package:flutter/material.dart';
+
+class ContinentTile extends StatelessWidget {
+  final Continent _continent;
+  final int _index;
+
+  const ContinentTile({@required Continent continent, @required int index})
+      : this._continent = continent,
+        this._index = index,
+        assert(continent != null),
+        assert(index != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+      child: Card(
+        elevation: 10.0,
+        child: MaterialButton(
+          onPressed: () {
+            
+          },
+          child: ListTile(
+            title: Text(
+              _continent.continent,
+              style: TextStyle(fontSize: 20.0),
+            ),
+            subtitle: Row(
+              children: <Widget>[
+                Chip(
+                  label: Text("Cases: ${_continent.cases}"),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Chip(
+                  label: Text("Deaths: ${_continent.deaths}"),
+                )
+              ],
+            ),
+            leading: Text(
+              "$_index",
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

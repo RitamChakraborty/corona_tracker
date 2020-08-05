36c0b5bab5a4f727e6fc603eae06d96fd8a02a1d
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-08-05 22:08:42 +0530

    Add search bar to continents page

--- lib/pages/continents_page.dart
+++ lib/pages/continents_page.dart
@@ -1,5 +1,6 @@
 import 'package:coronatracker/models/continent.dart';
 import 'package:coronatracker/providers/service_provider.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,23 +16,31 @@ class _ContinentsPageState extends State<ContinentsPage>
     ServiceProvider serviceProvider = Provider.of<ServiceProvider>(context);
     List<Continent> continents = serviceProvider.continents;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Continents"),
+    return Material(
+      child: SafeArea(
+        child: continents == null
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : FloatingSearchBar.builder(
+          leading: Icon(Icons.search),
+          trailing: IconButton(
+            icon: Icon(Icons.sort),
+            onPressed: null,
+          ),
+          padding:
+          const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+          decoration:
+          InputDecoration.collapsed(hintText: "Search country"),
+          itemCount: continents.length,
+          itemBuilder: (BuildContext context, int index) {
+            Continent continent = continents[index];
+            return ListTile(
+              title: Text("${continent.continent}"),
+            );
+          },
+        ),
       ),
-      body: continents == null ?
-          Center(
-            child: CircularProgressIndicator(),
-          ) : ListView.builder(
-        itemCount: continents.length,
-        itemBuilder: (BuildContext context, int index) {
-          Continent continent = continents[index];
-
-          return ListTile(
-            title: Text(continent.continent),
-          );
-        },
-      )
     );
   }
 

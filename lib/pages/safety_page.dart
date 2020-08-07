e27eb27b6576288bb60830aa5f97ccaeae50205e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-08-07 20:19:35 +0530

    Add header to safety page

--- lib/pages/safety_page.dart
+++ lib/pages/safety_page.dart
@@ -1,3 +1,4 @@
+import 'package:coronatracker/widgets/header.dart';
 import 'package:flutter/material.dart';
 
 class SafetyPage extends StatefulWidget {
@@ -9,11 +10,22 @@ class _SafetyPageState extends State<SafetyPage>
     with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Safety Measures"),
-      ),
-      body: Container(),
+    Size size = MediaQuery.of(context).size;
+
+    return Material(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverPersistentHeader(
+            delegate: Header(
+              maxExtent: size.height / 3,
+              minExtent: size.height / 4,
+            ),
+          ),
+          SliverFillRemaining(
+            child: Center(child: Text("Hello")),
+          )
+        ],
+      )
     );
   }
 

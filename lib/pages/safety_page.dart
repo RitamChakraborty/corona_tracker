ffeae88131a3eac7742107cba767b0d22655f0ec
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-08-07 21:03:02 +0530

    Create symptoms card and add to safety page

--- lib/pages/safety_page.dart
+++ lib/pages/safety_page.dart
@@ -1,4 +1,6 @@
+import 'package:coronatracker/services/symptoms_card.dart';
 import 'package:coronatracker/widgets/header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SafetyPage extends StatefulWidget {
@@ -11,24 +13,65 @@ class _SafetyPageState extends State<SafetyPage>
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    List<Widget> children = [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
+        child: Text(
+          "Symptoms",
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: SymptomsCard(
+                text: "Headache",
+                imagePath: 'assets/images/headache.png',
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: SymptomsCard(
+                text: "Cough",
+                imagePath: 'assets/images/caugh.png',
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: SymptomsCard(
+                text: "Fever",
+                imagePath: 'assets/images/fever.png',
+              ),
+            ),
+          ],
+        ),
+      )
+    ];
 
     return Material(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverPersistentHeader(
-            delegate: Header(
-              maxExtent: size.height / 3,
-              minExtent: size.height / 4,
-              imagePath: "assets/svgs/doctor_1.svg",
-              headerMessage: "If you feel sick,\nGet yourself tested!",
-            ),
+        child: CustomScrollView(
+      slivers: <Widget>[
+        SliverPersistentHeader(
+          delegate: Header(
+            maxExtent: size.height / 3,
+            minExtent: size.height / 4,
+            imagePath: "assets/svgs/doctor_1.svg",
+            headerMessage: "If you feel sick,\nGet yourself tested!",
           ),
-          SliverFillRemaining(
-            child: Center(child: Text("Hello")),
-          )
-        ],
-      )
-    );
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(children),
+        )
+      ],
+    ));
   }
 
   @override
---
+++ lib/services/symptoms_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SymptomsCard extends StatelessWidget {
+  final String _imagePath;
+  final String _text;
+
+  const SymptomsCard({@required String imagePath, @required String text})
+      : this._imagePath = imagePath,
+        this._text = text,
+        assert(imagePath != null),
+        assert(text != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 125,
+      width: 125,
+      margin: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 10.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Flex(
+            direction: Axis.vertical,
+            children: <Widget>[
+              Flexible(
+                flex: 4,
+                child: Image.asset(
+                  '$_imagePath',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Text("$_text"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
